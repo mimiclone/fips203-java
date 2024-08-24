f54e42d8455eef2b58e06421483494eabf5479fb
mimiclone <129893672+mimiclone@users.noreply.github.com>
2024-08-23 17:03:28 -0700

    Adding in development versions of the FIPS202 spec partial implementation to support the FIPS203 key generation.

---
+++ src/main/java/com/mimiclone/fips202/keccak/spi/Shake256StreamCipher.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2024 - Mimiclone, Inc. 
+ *
+
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mimiclone.fips202.keccak.spi;
+
+import com.mimiclone.fips203.shake.XOFParameterSet;
+import com.mimiclone.fips202.keccak.core.KeccakSponge;
+
+public class Shake256StreamCipher extends AbstractSpongeStreamCipher {
+
+	private KeccakSponge sponge;
+
+	@Override
+	KeccakSponge getSponge() {
+		if(sponge == null) {
+			sponge = new KeccakSponge(XOFParameterSet.SHAKE256);
+		}
+		return sponge;
+	}
+
+}
